7eef6e1415cffaa9799d92a7cd8b58e1ab432c7b
zhoukaijin <zhoukaijin@lakala.com>
2019-03-22 17:34:20 +0800

    monitor调试

--- cldt-cloud-monitor/src/main/java/com/cldt/front/monitor/SecurityConfig.java
+++ cldt-cloud-monitor/src/main/java/com/cldt/front/monitor/SecurityConfig.java
@@ -20,18 +20,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.headers().frameOptions().disable()
-				.and()
-				.formLogin()
-				.loginPage("/login.html")
-				.loginProcessingUrl("/login")
-				.and()
-				.logout().logoutUrl("/logout")
-				.and()
-				.csrf().disable()
-				.authorizeRequests()
-				.antMatchers("/api/**", "/applications/**", "/api/applications/**", "/login.html", "/**/*.css", "/img/**", "/third-party/**")
-				.permitAll()
-				.anyRequest().authenticated();
+		// Page with login form is served as /login.html and does a POST on
+		// /login
+		http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+		// The UI does a POST on /logout on logout
+		http.logout().logoutUrl("/logout");
+		// The ui currently doesn't support csrf
+		http.csrf().disable();
+
+		// Requests for the login page and the static assets are allowed
+		http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
+		// ... and any other request needs to be authorized
+		http.authorizeRequests().antMatchers("/**").authenticated();
+
+		// Enable so that the clients can authenticate via HTTP basic for
+		// registering
+		http.httpBasic();
 	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.headers().frameOptions().disable()
+//				.and()
+//				.formLogin()
+//				.loginPage("/login.html")
+//				.loginProcessingUrl("/login")
+//				.and()
+//				.logout().logoutUrl("/logout")
+//				.and()
+//				.csrf().disable()
+//				.authorizeRequests()
+//				.antMatchers("/api/**", "/applications/**", "/api/applications/**", "/login.html", "/**/*.css", "/img/**", "/third-party/**")
+//				.permitAll()
+//				.anyRequest().authenticated();
+//	}
 }
